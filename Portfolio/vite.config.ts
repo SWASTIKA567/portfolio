07a4e3e5ce6ef0a7c89ebeab1206bf66f48e3a02
SWASTIKA567 <babita.ayam2011@gmail.com>
2026-06-13 21:52:31 -0400

    add packages

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
     tailwindcss(),
+    tsconfigPaths(),
   ],
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
